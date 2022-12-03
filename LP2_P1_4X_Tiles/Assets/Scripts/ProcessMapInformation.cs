c3dd781bb928199f89adfb207fd62f18a14697cb
Pedro Oliveira <33415851+pedrooliveira2007@users.noreply.github.com>
2022-12-03 10:22:13 +0000

    ProcessMapInformation and GridHandler merged
    testing needed

--- LP2_P1_4X_Tiles/Assets/Scripts/ProcessMapInformation.cs
+++ LP2_P1_4X_Tiles/Assets/Scripts/ProcessMapInformation.cs
@@ -7,7 +7,16 @@ public class ProcessMapInformation : MonoBehaviour
     // Declaration of variables
     private string _rowsInfo, _colsInfo;
     private int _rows, _cols;
+    
+    // Declares Tile variable to house the _tilePrefab to be assigned in
+    // editor and instantiated later on
+    [SerializeField] private Tile _tilePrefab;
+    
+    // Declares List of type Tile to later on add all instantiated Tiles
+    private List<Tile> _tiles;
 
+    
+    
     /// <summary>
     /// Receives the information about the map and processes it
     /// </summary>
@@ -81,6 +90,41 @@ public class ProcessMapInformation : MonoBehaviour
         _tileInformation.RemoveAt(0);
 
         // Passes the list into a method to generate the map
-        // TODO
+        GenerateGrid(_rows, _cols, _tileInformation);
+        
     } 
+    
+    /// <summary>
+    /// Method used to generate grid of default Tiles, takes two integers
+    /// </summary>
+    /// <param name="_rows"> Desired rows on grid</param>
+    /// <param name="_cols"> Desired columns on grid</param>
+    private void GenerateGrid(int _rows, int _cols, List<string> tilesBlueprints)
+    {
+        // Initializes List of Tile to later on be used to easily refer back
+        // to instantiated Tiles
+        _tiles = new List<Tile>();
+        int line = 0;
+        // "For" loops run for the length of the number of rows and columns of
+        // the specified map
+        for (int y = 0; y < _rows; y++)
+        {
+            for (int x = 0; x < _cols; x++)
+            {
+                // Instantiates a new Tile Prefab
+                Tile spawnedTile = Instantiate(_tilePrefab, new Vector3(x, -y),
+                    Quaternion.identity);
+
+                // Changes the created Tile's name to include its coordinates on
+                // the grid
+                spawnedTile.name = $"Tile {y} {x}";
+
+                spawnedTile.InitializeTile(tilesBlueprints[line]);
+
+                // Adds the created Tile to the _tiles List
+                _tiles.Add(spawnedTile);
+                line++;
+            }
+        }   
+    }
 }
