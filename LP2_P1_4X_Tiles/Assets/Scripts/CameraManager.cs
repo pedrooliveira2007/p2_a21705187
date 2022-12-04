367bc32c0fcab9d60a6037285076424087b14b2d
AfonsoGR <afonso_rosa@hotmail.com>
2022-12-04 19:24:49 +0000

    Comments for CameraManager and replaced old legenda.png

--- LP2_P1_4X_Tiles/Assets/Scripts/CameraManager.cs
+++ LP2_P1_4X_Tiles/Assets/Scripts/CameraManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    // Declare variables
     [SerializeField] private Camera _cam;
     private float _cameraSpeed = 25.0f;
     private float _zoomSpeed = 10.0f;
@@ -14,75 +15,105 @@ public class CameraManager : MonoBehaviour
     /// </summary>
     void Awake()
     {
+        // Sets the initial camera position
         _cam.transform.position = new Vector3 (0.0f, 0.0f, -5.0f);
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
+        // Calls these 2 methods each Update
         Move();
         Zoom();
     }
 
+    /// <summary>
+    /// Assigns incoming Vector2 variable
+    /// </summary>
+    /// <param name="_boundariesPos"> Registers the last tiles' coordinates 
+    /// for the clamp </param>
     internal void ReceivePosition(Vector2 _boundariesPos)
     {
+        // Assigns value to local Vector2 variable
         _boundaries = _boundariesPos;
     }
 
+    /// <summary>
+    /// Manages the movement of the camera
+    /// </summary>
     private void Move()
     {
+        // Declares two variables to register input on the X and Y axis
         float _xinput = Input.GetAxis("Horizontal");
         float _yinput = Input.GetAxis("Vertical");
 
+        // Declares and assigns Vector3 variable to register the camera movement
         Vector3 _dir = _cam.transform.up * _yinput + 
             _cam.transform.right * _xinput;
 
+        // Moves the camera
         _cam.transform.position += _dir * _cameraSpeed * Time.deltaTime;
 
+        // Verification to halt camera movement if the limits are reached
         if (_cam.transform.position.x < 0)
         {
+            // Stops the camera from moving past a certain threshold
             _cam.transform.position = 
                 new Vector3(0, 
                 _cam.transform.position.y, _cam.transform.position.z);
         }
-        
+        // Verification to halt camera movement if the limits are reached
         if (_cam.transform.position.y > 0)
         {
+            // Stops the camera from moving past a certain threshold
             _cam.transform.position = 
                 new Vector3(_cam.transform.position.x, 0.0f, 
                 _cam.transform.position.z);
         }
-        
+        // Verification to halt camera movement if the limits are reached
         if (_cam.transform.position.x > _boundaries.x)
         {
+            // Stops the camera from moving past a certain threshold
             _cam.transform.position = 
             new Vector3(_boundaries.x, 
                 _cam.transform.position.y, _cam.transform.position.z);
         }
-        
+        // Verification to halt camera movement if the limits are reached
         if (_cam.transform.position.y < _boundaries.y)
         {
+            // Stops the camera from moving past a certain threshold
             _cam.transform.position = 
                 new Vector3(_cam.transform.position.x, _boundaries.y, 
                 _cam.transform.position.z);
         }
     }
 
+    /// <summary>
+    /// Manages the zoom of the camera
+    /// </summary>
     private void Zoom()
     {
+        // Declares a variable to register input in the Z axis
         float scrollInput = Input.GetAxis("Mouse ScrollWheel");
         
+        // Moves the camera
         _cam.transform.position += 
             _cam.transform.forward * scrollInput * _zoomSpeed;
 
+        // Verification to halt the camera from moving past a certain threshold
         if (_cam.transform.position.z > _minZoomDist)
         {
+            // Stops the camera from moving past a certain threshold
             _cam.transform.position = 
                 new Vector3(_cam.transform.position.x, 
                 _cam.transform.position.y, _minZoomDist);
         }
+        // Verification to halt the camera from moving past a certain threshold
         else if (_cam.transform.position.z < _maxZoomDist)
         {
+            // Stops the camera from moving past a certain threshold
             _cam.transform.position = 
             new Vector3(_cam.transform.position.x, 
                 _cam.transform.position.y, _maxZoomDist);
